58787a96300b0fa832676e2d01e934e30731ec23
Vitya Kravchenko <kravamasterk@gmail.com>
2021-11-10 19:05:04 +0200

    добавление блока в инедкс

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
-const div = document.getElementById('gitHub')
+"use strict";
 
 
-const logged = () => {
-    console.log(div.textContent);
-}
+
+let title = prompt("Как называется ваш проект?", "newproject");
+console.log(title);
+
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые");
+console.log(screens);
+
+let screenPrice = prompt("Сколько будет стоить данная работа?", "500");
+console.log(screenPrice);
+
+let adaptive = confirm("Нужен ли адаптив на сайте?","Да");
+console.log(!!"");
 
 
-console.log("Privat vasuli");
+let service1 = prompt("Какой дополнительный тип услуги нужен?","Корекция");
+console.log(service1);
+let servicePrice1 = prompt("Сколько это будет стоить?","100");
+console.log(servicePrice1);
+
+let service2 = prompt("Какой дополнительный тип услуги нужен?","Доставка");
+console.log(service2);
+let servicePrice2 = prompt("Сколько это будет стоить?","50");
+console.log(servicePrice2);
+
+let srvPrice = 10.5;
+console.log(parseFloat(srvPrice));
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2 + adaptive;
+console.log(fullPrice);
+
+
+let servicePresentPrice = confirm("servicePercentPrice = fullPrice - srvPrice");
+console.log(servicePresentPrice);
+
+
+
+/*
+esli (vuhodn) {radyemsya i spim;
+               potyagivaemsa;
+            }
+               inahe {
+                   ymuvaemsya;
+                   odevaemsya;
+                   rabitaem;
+               }
+
+*/
+
