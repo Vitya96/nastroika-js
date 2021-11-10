b861dd2182005770080cb079932b369ed2dbd15f
Vitya Kravchenko <kravamasterk@gmail.com>
2021-11-10 20:48:01 +0200

    доабавил вівод в конслоь

--- script.js
+++ script.js
@@ -1,51 +1,8 @@
-"use strict";
 
+let snakecase = 10;
+const width = 11;
 
-
-let title = prompt("Как называется ваш проект?", "newproject");
-console.log(title);
-
-let screens = prompt("Какие типы экранов нужно разработать?", "Простые");
-console.log(screens);
-
-let screenPrice = prompt("Сколько будет стоить данная работа?", "500");
-console.log(screenPrice);
-
-let adaptive = confirm("Нужен ли адаптив на сайте?","Да");
-console.log(!!"");
-
-
-let service1 = prompt("Какой дополнительный тип услуги нужен?","Корекция");
-console.log(service1);
-let servicePrice1 = prompt("Сколько это будет стоить?","100");
-console.log(servicePrice1);
-
-let service2 = prompt("Какой дополнительный тип услуги нужен?","Доставка");
-console.log(service2);
-let servicePrice2 = prompt("Сколько это будет стоить?","50");
-console.log(servicePrice2);
-
-let srvPrice = 10.5;
-console.log(parseFloat(srvPrice));
-
-let fullPrice = screenPrice + servicePrice1 + servicePrice2 + adaptive;
-console.log(fullPrice);
-
-
-let servicePresentPrice = confirm("servicePercentPrice = fullPrice - srvPrice");
-console.log(servicePresentPrice);
-
-
-
-/*
-esli (vuhodn) {radyemsya i spim;
-               potyagivaemsa;
-            }
-               inahe {
-                   ymuvaemsya;
-                   odevaemsya;
-                   rabitaem;
-               }
-
-*/
-
+const personAge = 20;
+console.log('personAge', personAge);
+console.log("commits")
+//izmeneniya
